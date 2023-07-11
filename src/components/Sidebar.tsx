5603732ce58d277c9f29c1f6d0002a421670da04
marettialine <alinesmaretti@gmail.com>
2023-07-11 01:33:19 -0300

    Atualização capa sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export function Sidebar(){
         <aside className = { styles.sidebar }>
             <img
                 className = { styles.cover }
-                src = "https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+                src = "https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
             />
         
             <div className = { styles.profile }>
